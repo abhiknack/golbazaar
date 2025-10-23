ee0f221471d867c678e57b025500a7db893baa30
Abhijeet <69189340+abhiknack@users.noreply.github.com>
2025-10-23 09:19:31 +0000

    fixed

--- golbazaar/hooks.py
+++ golbazaar/hooks.py
@@ -19,13 +19,13 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-add_to_apps_screen = [
-	{
-		"name": "golbazaar",
-		"title": "Golbazaar",
-		"route": "/golbazaar/dashboard"
-	}
-]
+# add_to_apps_screen = [
+# 	{
+# 		"name": "golbazaar",
+# 		"title": "Golbazaar",
+# 		"route": "/golbazaar/dashboard"
+# 	}
+# ]
 
 # Includes in <head>
 # ------------------
